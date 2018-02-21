86e2db778cca4c2685c9539f691964dadbba62e7
Linnea Wenall <linnea.wenall@gmail.com>
2018-02-21 08:18:40 +0100

    retried pushing

---
+++ problem1/src/main/java/com/cosylab/jwenall/academy/problem1/PowerSupply.java
@@ -0,0 +1,21 @@
+package com.cosylab.jwenall.academy.problem1;
+
+public interface PowerSupply {
+
+
+	/* Will turn the power on. */
+	public void on();
+
+	/* Will turn the power off. */
+	public void off();
+
+	/* Will reset the power supply to 0. */
+	public void reset();
+
+	/* Will return the current that power supply gives out. */
+	public double get();
+
+	/* Will set the current in the power supply to the given value. */
+	public void set(double value);
+
+}
