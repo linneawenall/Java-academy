a186107cdf56458aade236550346d958808b001f
Marko Novak <marko.novak@cosylab.com>
2018-03-14 15:12:31 +0100

    Made a review comment.

--- problem4/src/main/java/com/cosylab/jwenall/academy/problem4/RampedPowerSupplyImpl.java
+++ problem4/src/main/java/com/cosylab/jwenall/academy/problem4/RampedPowerSupplyImpl.java
@@ -2,6 +2,7 @@ package com.cosylab.jwenall.academy.problem4;
 
 import java.util.Arrays;
 
+// REVIEW (high): you are getting compilation errors here (you are missing PowerSupplyImpl file).
 public class RampedPowerSupplyImpl extends PowerSupplyImpl implements RampedPowerSupply {
 	private boolean ramping;
 	protected double[] rampValues;
